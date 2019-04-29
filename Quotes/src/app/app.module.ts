6c56bc09004540baf22f3d4cb52f2443c50aedd5
xystus ngigi <xystusngigi@gmail.com>
2019-04-29 19:40:59 +0300

    changes made

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DateCountPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
