cec9663dd84a0ac101c00ebcb0f748566749738a
xystus ngigi <xystusngigi@gmail.com>
2019-04-29 19:48:25 +0300

    made changes

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppComponent {
-  title = 'Quotes';
-}
+export class AppRoutingModule { }
